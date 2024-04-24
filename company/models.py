416784af49fc37f1f56858d43e052f23e4e693cb
Ankit Rai <raia85859@gmail.com>
2024-04-24 13:31:29 +0545

    adding features.........

--- company/models.py
+++ company/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Company(models.Model):
+    cname = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    instrument = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    website = models.URLField(max_length=100,blank=True)
+    slug = models.SlugField(max_length=100,unique=True,blank=True,null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.cname
--- company/views.py
+++ company/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Company
+from django.views import View
 # Create your views here.
+
+class BaseView(View):
+    context ={}
+
+
+class CompanyView(BaseView):
+    def get(self,request):
+        self.context['companies'] = Company.objects.all()
+        return render(request,'dashboard.html',self.context)
